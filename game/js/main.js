3259eea81a1b4bbe83ecf50dfeea7bd64ac04ea7
Paulo Penicheiro <smasher35@gmail.com>
2016-10-11 15:43:01 +0100

    correcção da URL(prof disse para tirar os :) iniciação de codificação dos HIGHLIGHT BUTTONS (ainda com bugs)

--- game/js/main.js
+++ game/js/main.js
@@ -12,6 +12,8 @@
         var cells = $('input'); //gets all the inputs
         var checkGameBtn = $('#btn-check'); //check game button
         var newGameBtn = $('#btn-new');
+        var highlightBtns = $('#highlightButtons button');
+        var timer;
 
 
         //function to initiate de game
@@ -20,6 +22,34 @@
             cells.prop("disabled", true);
             //callApiRest();
 
+        }
+
+
+        highlightBtns.click(function () {
+            clearInterval(timer);
+            timer = null;
+            highlightCells($(this).text());
+        });
+
+
+        function highlightCells(index) {
+            cells.each(function () {
+                if($(this).val() == index) {
+                    $(this).addClass('highlight');
+                }
+            });
+            if(timer == null || timer == undefined) {
+                timer = setInterval(function () {
+                    cells.each(function () {
+                        if($(this).val() == index) {
+                            $(this).removeClass('highlight');
+                        }
+                    });
+                },5000);
+
+            }
+            clearInterval(timer);
+            timer = null;
         }
 
 
@@ -35,7 +65,7 @@
 
         //adds the bakcground to a cell with value
         cells.blur(function() {
-            if($(this).val() > 0 && $(this).val() < 9) {
+            if($(this).val() > 0 && $(this).val() < 10) {
                 $(this).addClass('with-value');
             }
         });
@@ -53,16 +83,19 @@
             newGame();
         });
 
-
+        //starts a new game
         function newGame() {
             $('input').prop("disabled", false);
+            cells.each(function () {
+                $(this).val('').removeClass('with-value');
+            });
             //access the API to obtain the board
             callApiRest();
         }
         
         
         
-        //Processo Board with API REsults
+        //Process Board with API REsults
         function processBoard(line, col, value) {
             $("input[data-column='"+col +"'][data-line='"+line+"']").val(value);
 
@@ -79,16 +112,13 @@
                     processBoard(data[index].line, data[index].column, data[index].value);
                 });
             }
-
-
         }
 
         //call sudokuAPIRest
         function callApiRest() {
             var difficulty = $('#select-mode option:selected').val();
-            var url = 'http://198.211.118.123:8080/board/:';
-            var searchText = difficulty;
-            url += encodeURI(searchText);
+            var url = 'http://198.211.118.123:8080/board/';
+            url += encodeURI(difficulty);
             console.log(url);
             $.get(url, processAPIResults);
         }
