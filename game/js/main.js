b0a03e0013c7dd1d317bc5d9f45bd50d818423ab
Paulo Penicheiro <smasher35@gmail.com>
2016-10-11 10:53:54 +0100

    NewGame function added
    valida valores em todas as células
    insere backgroun na célula com valor à saída (blur)
    quando há duplo click nas células com valor, a célula em causa fica com borada vermelha

--- game/js/main.js
+++ game/js/main.js
@@ -8,11 +8,51 @@
     'use strict';
     $(document).ready(function () {
 
+        //Global variables
+        var cells = $('input'); //gets all the inputs
+        var checkGameBtn = $('#btn-check'); //check game button
+        var newGameBtn = $('#btn-new');
+
         //function to initiate de game
         function init() {
             projetAuthors();
+            cells.prop("disabled", true);
         }
 
+
+
+        //validates incorrect values on the input cells (deletes incorrect Values)
+        cells.keyup(function() {
+            if($(this).val() < 1 || $(this).val() > 9 || isNaN($(this).val())) {
+                $(this).val(' ');
+            }
+        });
+
+        //adds the bakcground to a cell with value
+        cells.blur(function() {
+            if($(this).val() > 0 && $(this).val() < 9) {
+                $(this).addClass('with-value');
+            }
+        });
+
+        //when a cell with a value is doubleclicked adds a red border
+        cells.dblclick(function() {
+            if($(this).val() > 0 && $(this).val() < 9) {
+                $(this).addClass('individual-highlight');
+            }
+        });
+
+
+        //process a new game
+        newGameBtn.click( function () {
+            newGame();
+        });
+
+
+        function newGame() {
+            $('input').prop("disabled", false);
+
+        }
 
         function projetAuthors() {
             var numbers = $('#authors-section h3');
