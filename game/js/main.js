176c133f54c06bc45521973654abcce5c321c240
Paulo Penicheiro <smasher35@gmail.com>
2016-10-11 13:46:26 +0100

    callAPIRest código corrigido

--- game/js/main.js
+++ game/js/main.js
@@ -28,6 +28,8 @@
         cells.keyup(function() {
             if($(this).val() < 1 || $(this).val() > 9 || isNaN($(this).val())) {
                 $(this).val(' ');
+            }else {
+                parseInt($(this).val());
             }
         });
 
@@ -73,7 +75,7 @@
             }else {
                 console.dir(data);
                 $.each(data, function(index, number){
-                    //primeiro parametro do each é o array, index posicao[0,1,2,3...] , number - elemento
+                    //params (Line  number, column number, cell value)
                     processBoard(data[index].line, data[index].column, data[index].value);
                 });
             }
@@ -83,8 +85,8 @@
 
         //call sudokuAPIRest
         function callApiRest() {
-            var difficulty = $('#select-mode option:selected').text();
-            var url = 'http://198.211.118.123:8080/board/:mode ';
+            var difficulty = $('#select-mode option:selected').val();
+            var url = 'http://198.211.118.123:8080/board/:';
             var searchText = difficulty;
             url += encodeURI(searchText);
             console.log(url);
